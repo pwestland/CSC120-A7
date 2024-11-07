66493def0d979639ee93c7ad41cf5170870759a9
pwestland <pwestland@smith.edu>
2024-11-06 20:20:14 -0500

    fixed cafe and building elevators

--- CampusMap.java
+++ CampusMap.java
@@ -51,6 +51,7 @@ public class CampusMap {
         myMap.addHouse(new House("Morris House", "101 Green St Northampton, MA 01063", 4, false, false));
         myMap.addHouse(new House("Tyler House", "164 Green St Northampton, MA 01063", 3, true, false));
         myMap.addBuilding(new Building("Schacht Center for Health and Wellness", "21 Belmont Ave Northampton, MA 01063", 3, true));
+        myMap.addBuilding(new Building("Smith College Museum of Art", "20 Elm St at Bedford Terrace Northampton, MA 01063", 4));
         myMap.addCafe(new Cafe("Compass Cafe", "7 Neilson Dr Northampton, MA 01063", 1, false));
         System.out.println(myMap);
     }
